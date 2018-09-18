5e42ce354622de62e897ce7528d9de4079ce984d
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-09-18 12:05:52 +0530

    Initial Commit - Web APP and Web API

---
+++ web-api/web-api/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿
+namespace web_api
+{
+    using System.Web.Http;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+        }
+    }
+}
