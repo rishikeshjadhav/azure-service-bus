8d31df997260c8529e1c3b4c3e0e089be2b6ecab
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-09-19 10:44:37 +0530

    Adds Service Bus API project and Ping method

---
+++ web-api/service-bus-api/ServiceBusController.cs
@@ -0,0 +1,15 @@
+﻿
+namespace service_bus_api
+{
+    using System.Web.Http;
+
+    [RoutePrefix("api/servicebus")]
+    public class ServiceBusController : ApiController
+    {
+        [HttpGet, Route("ping")]
+        public IHttpActionResult Ping()
+        {
+            return Ok("Ping successful");
+        }
+    }
+}
