3b8b162c4a33d85337f42b91169211dc97add0b0
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-09-26 10:25:02 +0530

    Adds shared services and sample API get call

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './shared/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-app';
+
+  constructor(private dataService: DataService) {
+
+  }
+
+  ngOnInit() {
+    console.log("Inside app component");
+    this.dataService.get('')
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
 }
---
+++ web-app/src/app/shared/constants.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConstantsService {
+
+  constructor() { }
+
+  public static URL = {
+    API: 'https://localhost:44308/'
+  };
+}
